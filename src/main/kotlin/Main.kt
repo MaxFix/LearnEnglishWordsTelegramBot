76a7ccc0ab16fe360256de77100843e0b1a8e5ac
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-08-03 20:57:21 +0300

    EnglishTelegramBot, Этап 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,11 +12,37 @@ fun main(args: Array<String>) {
     }
 
     while (true) {
-        val correctAnswersCount = dictionary.filter { it.learned == 3 }.size
+        val correctAnswersCount = dictionary.filter { (it.learned == 3) }.size
         val correctAnswersPercent = (correctAnswersCount * 100) / wordsFile.size
         println("Введите 1 - Учить слова, 2 - Статистика, 0 - Выход")
         when (readln().toInt()) {
-            1 -> println("Учить слова")
+            1 -> {
+                val unlearnedWords = dictionary.filter { it.learned != 3 }
+
+                if (unlearnedWords.isNotEmpty()) {
+                    val unlearnedWord = unlearnedWords.random()
+                    val selectedValues = mutableListOf<String>()
+
+                    println("Исходное слово: ${unlearnedWord.original}")
+
+                    while (selectedValues.size < 3) {
+                        val answer = unlearnedWords.random().translate
+                        if (answer != unlearnedWord.translate && answer !in selectedValues) {
+                            selectedValues.add(answer)
+                        }
+                    }
+
+                    selectedValues.add(unlearnedWord.translate)
+                    selectedValues.shuffle()
+                    selectedValues.forEachIndexed { index, value ->
+                        val number = index + 1
+                        println("Ответ ${number}: $value")
+                    }
+                } else {
+                    println("Все слова выучены!")
+                    break
+                }
+            }
             2 -> println("Статистика: Выучено $correctAnswersCount из ${wordsFile.size} | ${correctAnswersPercent}%")
             0 -> break
             else -> println("Предупреждение! Вводить можно только 0, 1 или 2")
