5db1782d40c6b4fbcd164a8484d5d5db3adf0c05
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-08-08 00:33:55 +0300

    EnglishTelegramBot, Этап 6

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,48 +1,21 @@
-import java.io.File
-
 const val MAX_LEARNED_COUNTER = 3
 const val NUM_OF_SELECTED_WORDS = 4
 
 fun main(args: Array<String>) {
 
-    val dictionary = mutableListOf<Word>()
-    val wordsFile: List<String> = File("some_words.txt")
-        .readLines()
-
-    wordsFile.forEach {
-        val line = it.split("|")
-        val word = Word(original = line[0], translate = line[1], learned = line[2].toInt())
-        dictionary.add(word)
-    }
-
-    fun saveDictionary(dictionary: MutableList<Word>) {
-        val fileWithWordsDictionary = File("some_words.txt")
-        fileWithWordsDictionary.writeText("")
-        dictionary.forEach {
-            val word = Word(original = it.original, translate = it.translate, learned = it.learned)
-            fileWithWordsDictionary.appendText("${word.original}|${word.translate}|${word.learned}\n")
-        }
-    }
+    val trainer = WordsTrainer()
 
     while (true) {
-        val correctAnswersCount = dictionary.filter { it.learned == MAX_LEARNED_COUNTER }.size
-        val correctAnswersPercent = (correctAnswersCount * 100) / wordsFile.size
+
         println("Введите 1 - Учить слова, 2 - Статистика, 0 - Выход")
         when (readln().toInt()) {
             1 -> {
-                val unlearnedWords = dictionary.filter { it.learned < MAX_LEARNED_COUNTER }
+                val question = trainer.getNextQuestion()
 
-                if (unlearnedWords.isNotEmpty()) {
-                    var learnedWords = dictionary.filter { it.learned >= MAX_LEARNED_COUNTER }
-                    val selectedValues = unlearnedWords.shuffled().take(NUM_OF_SELECTED_WORDS)
-                    if (selectedValues.size < NUM_OF_SELECTED_WORDS) {
-                        learnedWords = learnedWords.shuffled().take(NUM_OF_SELECTED_WORDS - selectedValues.size)
-                    }
+                if (question != null) {
+                    println("Исходное слово: ${question.correctAnswer.original}")
 
-                    val unlearnedWord = selectedValues.random().original
-                    println("Исходное слово: $unlearnedWord")
-
-                    val allWords = unlearnedWords + learnedWords
+                    val allWords = question.variants
                     allWords.forEachIndexed { index, value ->
                         val number = index + 1
                         println("${number}: ${value.translate}")
@@ -52,12 +25,10 @@ fun main(args: Array<String>) {
                     val userInput = readln().toInt()
                     if (userInput == 0) continue
 
-                    val answerIndex = allWords.indexOfFirst { it.original == unlearnedWord } + 1
-                    if (userInput == answerIndex) {
+                    if (trainer.checkAnswer(userInput.minus(1))) {
                         println("Верно!")
-                        val selectedWord = allWords.find { it.original == unlearnedWord }
-                        selectedWord?.learned = selectedWord?.learned?.plus(1)!!
-                        saveDictionary(dictionary)
+                    } else {
+                        println("Неправильно! ${question.correctAnswer.original} - это ${question.correctAnswer.translate}")
                     }
                 } else {
                     println("Вы выучили все слова")
@@ -65,7 +36,14 @@ fun main(args: Array<String>) {
                 }
             }
 
-            2 -> println("Статистика: Выучено $correctAnswersCount из ${wordsFile.size} | ${correctAnswersPercent}%")
+            2 -> {
+                val statistics = trainer.getStatistics()
+                println(
+                    "Статистика: Выучено ${statistics.correctAnswersCount} из " +
+                            "${statistics.totalAnswers} | ${statistics.percent}%"
+                )
+            }
+
             0 -> break
             else -> println("Предупреждение! Вводить можно только 0, 1 или 2")
         }
---
+++ src/main/kotlin/WordsTrainer.kt
@@ -0,0 +1,83 @@
+import java.io.File
+
+data class Statistics(
+    val correctAnswersCount: Int,
+    val totalAnswers: Int,
+    val percent: Int,
+)
+
+data class Question(
+    val variants: List<Word>,
+    val correctAnswer: Word,
+)
+
+class WordsTrainer {
+
+    private var question: Question? = null
+    private val dictionary = loadDictionary()
+    private val wordsFile = File("some_words.txt")
+
+    private fun loadDictionary(): List<Word> {
+        val wordsFile = File("some_words.txt").readLines()
+        val dictionary = mutableListOf<Word>()
+
+        wordsFile.forEach {
+            val line = it.split("|")
+            val word = Word(original = line[0], translate = line[1], learned = line[2].toInt())
+            dictionary.add(word)
+        }
+        return dictionary
+    }
+
+    private fun saveDictionary(dictionary: List<Word>) {
+        wordsFile.writeText("")
+        dictionary.forEach {
+            val word = Word(original = it.original, translate = it.translate, learned = it.learned)
+            wordsFile.appendText("${word.original}|${word.translate}|${word.learned}\n")
+        }
+    }
+
+    fun getStatistics(): Statistics {
+        val correctAnswersCount = dictionary.filter { it.learned == MAX_LEARNED_COUNTER }.size
+        val totalAnswers = dictionary.size
+        val correctAnswersPercent = (correctAnswersCount * 100) / totalAnswers
+        return Statistics(
+            correctAnswersCount,
+            totalAnswers,
+            correctAnswersPercent
+        )
+    }
+
+    fun getNextQuestion(): Question? {
+        var learnedWords = dictionary.filter { it.learned >= MAX_LEARNED_COUNTER }
+        val unlearnedWords = dictionary.filter { it.learned < MAX_LEARNED_COUNTER }
+        if (unlearnedWords.isEmpty()) return null
+        val selectedValues = unlearnedWords.shuffled().take(NUM_OF_SELECTED_WORDS)
+
+        if (selectedValues.size < NUM_OF_SELECTED_WORDS) {
+            learnedWords = learnedWords.shuffled().take(NUM_OF_SELECTED_WORDS - selectedValues.size)
+        }
+
+        val unlearnedWord = selectedValues.random()
+
+        return Question(
+            variants = unlearnedWords + learnedWords,
+            correctAnswer = unlearnedWord,
+        )
+    }
+
+    fun checkAnswer(userAnswer: Int?): Boolean {
+        return question?.let { it ->
+            val answerIndex = it.variants.indexOf(it.correctAnswer)
+
+            if (userAnswer == answerIndex) {
+                val selectedWord = it.variants.find { it.original == getNextQuestion()?.correctAnswer?.original }
+                selectedWord?.learned = selectedWord?.learned?.plus(1)!!
+                saveDictionary(dictionary)
+                true
+            } else {
+                false
+            }
+        } ?: false
+    }
+}
