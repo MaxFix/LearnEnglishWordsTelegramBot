77fa04a5dae081e2747a1686174f861397e4047c
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-08-11 02:11:18 +0300

    EnglishTelegramBot, Этап 8

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -14,13 +14,11 @@ fun main(args: Array<String>) {
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-
-        updateId = updateIdString.toInt() + 1
+        val regex = """^(?:"update_id":\s)(\d+)(?:,)""".toRegex()
+        val matchResult = regex.find(updates)
+        if (matchResult != null) {
+            updateId = matchResult.groupValues[1].toInt() + 1
+        }
     }
 }
 
