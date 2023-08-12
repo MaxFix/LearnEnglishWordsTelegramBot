6845c04f8a65991e62ad8f2e394e88452f12d8e4
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-08-12 22:30:30 +0300

    Попытка сделать отправку сообщения из бота на текст Hello

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -7,18 +7,38 @@ fun main(args: Array<String>) {
 
     val botToken = args[0]
     var updateId = 0
+    var chatId = 0
+    var text = ""
 
 
     while (true) {
-        Thread.sleep(2000)
+        Thread.sleep(1000)
         val updates: String = getUpdates(botToken, updateId)
         println(updates)
 
-        val regex = "\"update_id\":(.+?),".toRegex()
-        val matchResult = regex.find(updates)
-        if (matchResult != null) {
-            updateId = matchResult.groupValues[1].toInt() + 1
+        val regexUpdateId = "\"update_id\":(.+?),".toRegex()
+        val regexChatId = "\"chat\":\\{\"id\":(.+?),".toRegex()
+        val regexText = "\"text\":(.+?)}".toRegex()
+
+        val matchResultUpdateId = regexUpdateId.find(updates)
+        val matchResultChatId = regexChatId.find(updates)
+        val matchResultText = regexText.find(updates)
+
+        if (matchResultUpdateId != null) {
+            updateId = matchResultUpdateId.groupValues[1].toInt() + 1
         }
+        if (matchResultChatId != null) {
+            chatId = matchResultChatId.groupValues[1].toInt()
+        }
+
+        if (matchResultText != null) {
+            text = matchResultText.groupValues[1]
+        }
+
+        if (text.lowercase() == "hello") {
+            sendMessage(botToken, chatId, "Hello")
+        }
+
     }
 }
 
@@ -27,6 +47,23 @@ fun getUpdates(botToken: String, updateId: Int): String {
 
     val client: HttpClient = HttpClient.newBuilder().build()
     val request = HttpRequest.newBuilder().uri(URI.create(urlUpdate)).build()
+    val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+    return response.body()
+}
+
+
+fun sendMessage(botToken: String, chatId: Int, message: String): String {
+    val urlSendMessage = "https://api.telegram.org/bot$botToken/sendMessage"
+
+    val requestBody = mapOf("chat_id" to chatId, "text" to message)
+    val requestBodyString = requestBody.entries.joinToString("&") { "${it.key}=${it.value}" }
+    val client: HttpClient = HttpClient.newBuilder().build()
+    val request = HttpRequest
+        .newBuilder()
+        .uri(URI.create(urlSendMessage))
+        .POST(HttpRequest.BodyPublishers.ofString(requestBodyString))
+        .build()
     val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
 
     return response.body()
